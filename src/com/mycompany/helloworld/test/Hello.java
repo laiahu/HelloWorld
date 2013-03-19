f61eef1ae4b3e82aaf9df4ae238af2ae415484e7
xiaohu <laiahu@126.com>
2013-03-19 15:41:33 +0800

    update ssh

--- src/com/mycompany/helloworld/test/Hello.java
+++ src/com/mycompany/helloworld/test/Hello.java
@@ -3,6 +3,6 @@ package com.mycompany.helloworld.test;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("this is hello 2");
+		System.out.println("this is hello!");
 	}
 }
